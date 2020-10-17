9cf0b832523e4ccb754755d33497eaa71612fe4b
“Ciara <60624812+ciaraduggan@users.noreply.github.com>
2020-10-17 15:49:55 -0400

    commit

--- duggan_final_project_gov50/app.R
+++ duggan_final_project_gov50/app.R
@@ -1,64 +1,66 @@
 
 # GOV 50 Final Project Shiny App
 
+# Load libraries
+
 library(shiny)
+
 library(shinythemes)
 
-# Define UI for application that draws a lineplot
+# Load data
+
+county_drug_data <- readRDS("/Users/ciaraduggan/Desktop/gov_50/final_project/duggan_final_project_gov50/data/county_drug_data.rds")
+
+# Define UI
 ui <- navbarPage(
-    "Final Project Title",
+    "Opioid Prescription and Drug-Related Death Trends in the U.S.",
     tabPanel("Model",
              fluidPage(theme = shinytheme("cerulean"),
-                 titlePanel("Model Title"),
+                 titlePanel("Model"),
                  sidebarLayout(
                      sidebarPanel(
+                         helpText("Choose a year."),
                          selectInput(
-                             "plot_type",
-                             "Plot Type",
-                             c("Option A" = "a", "Option B" = "b")
+                             "year",
+                             label = "Year",
+                             choices = c("2006", "2007", "2008", "2009", "2010", "2011",
+                               "2012", "2013", "2014", "2015", "2016", "2017"),
+                             selected = "2017"
                          )),
-                     mainPanel(plotOutput("line_plot")))
+                     mainPanel(plotOutput("scatterPlot")))
              )),
+    
     tabPanel("Discussion",
              titlePanel("Discussion Title"),
              p("Tour of the modeling choices I made and 
               an explanation of why I made them.")),
+    
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
              p("Hello, this is where I talk about my project. 
                Discuss data sources here for Milestone 4."),
-             p("insert repo url"),
+             p("My Github repository can be found here: 
+               https://github.com/ciaraduggan/final_project"),
              h3("About Me"),
              p("My name is Ciara Duggan and I am a senior at Harvard College 
              concentrating in Social Studies with a secondary field in Global 
              Health and Health Policy. 
              You can reach me at ciaraduggan@college.harvard.edu.")))
 
-# Define server logic required to draw a lineplot
+# Define server logic 
+
 server <- function(input, output) {
-    output$line_plot <- renderPlot({
-        # Generate type based on input$plot_type from ui
-        
-        ifelse(
-            input$plot_type == "a",
-            
-            # If input$plot_type is "a", plot histogram of "waiting" column 
-            # from the faithful dataframe
-            
-            x   <- faithful[, 2],
-            
-            # If input$plot_type is "b", plot histogram of "eruptions" column
-            # from the faithful dataframe
-            
-            x   <- faithful[, 1]
-        )
-        
-        # Draw the histogram with the specified number of bins
-        
-        hist(x, col = 'darkgray', border = 'white')
+    output$scatterPlot <- renderPlot({ 
+        county_drug_data %>%
+            filter(YEAR == input$year) %>%
+            ggplot(aes(x = opioid_RxRate, y = drugdeathrate_est)) +
+            geom_point(alpha = 0.1) +
+            geom_smooth(method = lm) +
+            labs(title = "title", x = "x label", y = "y label")
     })
 }
 
 # Run the application 
+
 shinyApp(ui = ui, server = server)
---
+++ duggan_final_project_gov50/data/gather.R
@@ -0,0 +1,69 @@
+
+# Setting up file and loading libraries
+library(tidyverse)
+library(readr)
+
+
+# Loading AmfAR county-level opioid Rx rate dataset (2006-2017).
+
+county_rx_rate <- 
+  read_csv(file = "/Users/ciaraduggan/Desktop/gov_50/final_project/duggan_final_project_gov50/raw_data/county_opioid_RxRate.csv", 
+           col_types = cols(
+             STATE = col_character(),
+             STATEABBREVIATION = col_character(),
+             COUNTY = col_character(),
+             STATEFP = col_character(),
+             COUNTYFP = col_character(),
+             INDICATOR = col_character(),
+             YEAR = col_character(),
+             VALUE = col_double()
+           ))
+
+# Loading AmfAR data on county-level age-adjusted drug-related death rate (per
+# 100,000).
+
+county_drug_deaths <-
+  read_csv(file = "/Users/ciaraduggan/Desktop/gov_50/final_project/duggan_final_project_gov50/raw_data/county_ageadjusted_drugdeathrate.csv", 
+           col_types = cols(
+             STATE = col_character(),
+             STATEABBREVIATION = col_character(),
+             COUNTY = col_character(),
+             STATEFP = col_character(),
+             COUNTYFP = col_character(),
+             INDICATOR = col_character(),
+             YEAR = col_character(),
+             VALUE = col_double()
+           ))
+
+# Pivoting the county_drug_deaths dataset to a wider format to create a variable
+# containing age-adjusted drug-related death rate estimates.
+
+county_drug_deaths_wider <-
+  county_drug_deaths %>%
+  pivot_wider(names_from = "INDICATOR", values_from = "VALUE")
+
+# Pivoting the county_rx_rate_wider dataset to a wider format to create a
+# variable containing the opioid prescription rates per 100,000 people.
+
+county_rx_rate_wider <-
+  county_rx_rate %>%
+  pivot_wider(names_from = "INDICATOR", values_from = "VALUE")
+
+# Joining the resulting county_drug_deaths_wider and county_rx_rate_wider data
+# sets by county and year.
+
+county_drug_data <- county_drug_deaths_wider %>%
+  inner_join(county_rx_rate_wider, by = c("COUNTY", 
+                                          "YEAR", 
+                                          "STATE", 
+                                          "STATEABBREVIATION",
+                                          "STATEFP",
+                                          "COUNTYFP"
+  ))
+
+# Saving county_drug_data as an rds file.
+
+write_rds(county_drug_data, "county_drug_data.rds")
+
+
+
